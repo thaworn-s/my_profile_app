7e03d8f88bd758635b0ab8be3fb3dbdf61bcd991
thaworn-s <s6319c10030>
2022-07-17 16:18:38 +0700

    instsll package and create UI

---
+++ lib/views/your_phone_ui.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class YourPhoneUI extends StatefulWidget {
+  const YourPhoneUI({Key? key}) : super(key: key);
+
+  @override
+  State<YourPhoneUI> createState() => _YourPhoneUIState();
+}
+
+class _YourPhoneUIState extends State<YourPhoneUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+    );
+  }
+}
